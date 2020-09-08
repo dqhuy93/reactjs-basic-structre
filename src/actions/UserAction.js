73e69672cafc561e7ad4de4e741cf390b3fbea79
Huy Dang <huy.dang@exe.com.vn>
2020-09-08 23:32:02 +0700

    implement feature cancel request + debounce input using axios and lodash.debounce

--- src/actions/UserAction.js
+++ src/actions/UserAction.js
@@ -1,22 +1,44 @@
 import { fetchApi } from '../utils/apiCaller';
 import { user } from '../constants/actions';
 
-export const fetchUserSuccess = payload => {
+export const fetchUserSuccess = (payload) => {
   return {
     type: user.FETCH_USER_SUCCESS,
-    payload
+    payload,
   };
 };
 
 export const actGetUser = () => {
-  return async dispatch => {
+  return async (dispatch) => {
     return fetchApi('https://jsonplaceholder.typicode.com/users', 'GET').then(
-      response => {
+      (response) => {
         dispatch(fetchUserSuccess(response.data));
       },
-      error => {
+      (error) => {
         console.log(error);
       }
     );
   };
+};
+
+/**
+ *
+ * @param {string} value : value input typing
+ * fake api delay 4s
+ * http://slowwly.robertomurray.co.uk/delay/3000/url/https://jsonplaceholder.typicode.com/posts/1
+ */
+export const mockApiDelay = async (value, tokenCancel) => {
+  // value : chứa giá trị của input
+
+  // randomId để call mock api, chỉ để test cho dễ hiểu
+  const randomId = Math.floor(Math.random() * 100) + 1;
+
+  return fetchApi(
+    'http://slowwly.robertomurray.co.uk/delay/4000/url/https://jsonplaceholder.typicode.com/posts/' +
+      randomId,
+    'GET',
+    null,
+    null,
+    tokenCancel
+  );
 };
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,46 +1,97 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { actGetUser } from '../../actions';
+import { mockApiDelay } from '../../actions';
+import debounce from 'lodash.debounce';
+import axios from 'axios';
+import { Input, Spin } from 'antd';
+
 import './home.scss';
 
+// https://reactjs.org/docs/faq-functions.html#debounce
+// https://github.com/axios/axios#cancellation
+
 class Home extends Component {
-  componentDidMount() {
-    this.props.onGetUser();
+  constructor(props) {
+    super(props);
+
+    // hoãn thời gian 200ms,
+    // khi user gõ phím (khoảng cách giữa 2 lần gõ liên tục < 200ms) thì sẽ ko excute function this.emitChange
+    // ngược lại thì excute function this.emitChange
+    this.emitChangeDebounced = debounce(this.emitChange, 200);
+
+    this.state = {
+      userDetail: null,
+      isLoading: false,
+    };
   }
 
-  render() {
-    const { users } = this.props;
+  componentWillUnmount() {
+    // hủy debounce (ko thực thi hàm this.emitChange nếu như trong thời gian api pending mà user rời khỏi component)
+    this.emitChangeDebounced.cancel();
 
+    // hủy request nếu như trong thời gian api pending mà user rời khỏi component
+    this.signal.cancel('Canceled');
+  }
+
+  handleChange = (e) => {
+    // hủy request đang nếu như đang pending
+    if (this.signal) {
+      this.signal.cancel('Canceled');
+    }
+
+    // tạo token mới cho request tiếp theo
+    this.signal = axios.CancelToken.source();
+
+    // emit debounce change
+    this.emitChangeDebounced(e.target.value);
+  };
+
+  emitChange = async (value) => {
+    // console.log(value);
+    try {
+      // show loading
+      this.setState({
+        isLoading: true,
+      });
+
+      // pass value và tokenCancel vào hàm mockApiDelay
+      const response = await mockApiDelay(value, this.signal.token);
+
+      // nếu kết quả thành công thì setState
+      if (response && response.data) {
+        this.setState({
+          userDetail: response.data,
+        });
+      }
+    } catch (error) {}
+
+    // hide loading
+    this.setState({
+      isLoading: false,
+    });
+  };
+
+  render() {
+    const { userDetail, isLoading } = this.state;
     return (
       <div className="demo">
         <h1>This is Home Page</h1>
         <h2>Example call API</h2>
+        <Input onChange={this.handleChange} />
         <br />
-        <ul>
-          {users.map(user => (
-            <li key={user.id}>
-              <strong>{user.name}</strong>
-              <div>{user.email}</div>
-              <br />
-            </li>
-          ))}
-        </ul>
+        <br />
+        <Spin spinning={isLoading} size="large" />
+        <br />
+        {userDetail ? (
+          <pre style={{ background: '#CCC', padding: 10 }}>
+            {JSON.stringify(userDetail, null, 2)}
+          </pre>
+        ) : null}
+
+        <br />
       </div>
     );
   }
 }
 
-function mapStateToProps(state) {
-  return { users: state.users };
-}
-function mapDispatchToProps(dispatch) {
-  return {
-    onGetUser: () => {
-      dispatch(actGetUser());
-    }
-  };
-}
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Home);
+export default connect(null, null)(Home);
